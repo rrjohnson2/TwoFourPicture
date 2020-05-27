faa56d903edaca7bd601b2cc17da2f148805037f
Roice Johnson <rjohn230uncc.edu>
2020-05-27 10:42:58 -0400

    works

--- app.js
+++ app.js
@@ -14,22 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
-
-var profile_picture_storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/images')
-    },
-    filename: (req, file, cb) => {
-        if(path.extname(file.originalname)==".png"){ 
-            cb(null,file.originalname)}
-        else
-           { 
-               cb(new Error('Only png are allowed'));
-            }
-    }
-});
-
-var bit_storage = multer.diskStorage({
+var twofour_storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, './public/content')
     },
@@ -48,30 +33,8 @@ var bit_storage = multer.diskStorage({
     }
 });
 
-
-var twofour_storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/twofour')
-    },
-    filename: (req, file, cb) => {
-        console.log(file);
-        if(path.extname(file.originalname)==".mp3"||
-          path.extname(file.originalname)==".png" ||
-          path.extname(file.originalname)==".jpg" ||
-          path.extname(file.originalname)==".mp4"
-          ){
-            cb(null,file.originalname)}
-        else
-           { 
-               cb(new Error('Only audio video and images are allowed'));
-            }
-    }
-});
-
 //will be using this for uplading
-const profile_picture = multer({ storage: profile_picture_storage }).single('avatar');
-const bit_content = multer({ storage: bit_storage }).single('content');
-const twofour_content = multer({ storage: twofour_storage }).single('content');
+const twofour_content = multer({ storage: twofour_storage }).single('sub');
 
 
 app.use(logger('tiny'));
@@ -82,29 +45,13 @@ app.use(cors());
 //app.use(express.static('public'));
 app.use('/ftp', express.static('public'), serveIndex('public', {'icons': true}));
 
-app.get('/', function(req,res) {
-    return res.send("hello from my app express server!")
-})
-app.get('/avatar', function(req,res) {
-    var user = req.param("user");
-    res.sendFile(path.join(`${__dirname}/public/images/${user}`));
-})
-app.get('/content', function(req,res) {
-    var content = req.param("content");
-    res.sendFile(path.join(`${__dirname}/public/content/${content}`));
+
+app.get('/getsubmission', function(req,res) {
+    var sub = req.param("sub");
+    res.sendFile(path.join(`${__dirname}/public/content/${sub}`));
 })
 
-app.post('/upload_profile_picture', function(req,res) {
-    profile_picture(req,res,(err)=> {
-        res.send(err)});
-})
-
-app.post('/upload_content', function(req,res) {
-    bit_content(req,res,(err)=> {
-        res.send(err)});
-})
-
-app.post('/twofour', function(req,res) {
+app.post('/uploadsubmission', function(req,res) {
     twofour_content(req,res,(err)=> {
         res.send(err)});
 })
